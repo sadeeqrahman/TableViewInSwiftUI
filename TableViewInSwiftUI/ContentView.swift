7dc9b79507cd56eeffdd2fc2875e2e2b8333a4f7
Sadeeq Rahman <35709246+sadeeqrahman@users.noreply.github.com>
2022-03-22 22:43:00 +0500

    Initial Commit

---
+++ TableViewInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableViewInSwiftUI
+//
+//  Created by Sadeeq Rahman on 22/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TableViewInSwiftUI/TableViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewInSwiftUIApp.swift
+//  TableViewInSwiftUI
+//
+//  Created by Sadeeq Rahman on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TableViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
